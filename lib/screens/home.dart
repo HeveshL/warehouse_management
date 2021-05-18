8ef88289abac5d7f55f18c378f28e6a3948dc181
Hevesh <hevesh000@gmail.com>
2021-05-18 19:30:26 +0530

    Homepage initialized

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:warehouse_management/utils/color_palette.dart';
 
 class Home extends StatelessWidget {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: IconButton(
-          icon: const Icon(Icons.logout),
-          onPressed: () async {
-            await firebaseAuth.signOut();
-          },
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 10,
+          right: 10,
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          splashColor: ColorPalette.bondyBlue,
+          backgroundColor: ColorPalette.pacificBlue,
+          child: const Icon(Icons.add,color: ColorPalette.white,),
+        ),
+      ),
+      body: Container(
+        color: ColorPalette.pacificBlue,
+        child: SafeArea(
+          child: Container(
+            color: ColorPalette.aquaHaze,
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    left: 20,
+                    right: 15,
+                  ),
+                  width: double.infinity,
+                  height: 90,
+                  decoration: const BoxDecoration(
+                    color: ColorPalette.pacificBlue,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Homepage",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 28,
+                          color: ColorPalette.timberGreen,
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.search,
+                          color: ColorPalette.timberGreen,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: SizedBox(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: const [
+                                SizedBox(
+                                  height: 20,
+                                ),
+                              ],
+                            ),
+                            const Text(
+                              "Product Groups",
+                              style: TextStyle(
+                                color: ColorPalette.timberGreen,
+                                fontSize: 20,
+                                fontFamily: "Nunito",
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
